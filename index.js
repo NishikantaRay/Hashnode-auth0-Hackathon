2a4069462c2bb9a701e19b9c0c24d7aff18bb07d
Nishikantaray <nishikantaray1@gmail.com>
2022-02-05 00:26:47 +0530

    update

--- index.js
+++ index.js
@@ -826,4 +826,27 @@ String.prototype.trim = function (char) {
   return this.replace(/^\s+|\s+$/g, '');
 };
 
+$('.counting').each(function() {
+  var $this = $(this),
+      countTo = $this.attr('data-count');
+  
+  $({ countNum: $this.text()}).animate({
+    countNum: countTo
+  },
 
+  {
+
+    duration: 3000,
+    easing:'linear',
+    step: function() {
+      $this.text(Math.floor(this.countNum));
+    },
+    complete: function() {
+      $this.text(this.countNum);
+      //alert('finished');
+    }
+
+  });  
+  
+
+});
